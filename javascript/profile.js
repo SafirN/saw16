b68d4cc562f675d2cb3356d292fac60d63b4be22
Safir <safir.najafi@gmail.com>
2016-03-29 21:43:21 +0200

    funkareee?

--- javascript/profile.js
+++ javascript/profile.js
@@ -1,3 +1,10 @@
 function loadProfile() {
-	document.getElementById("content").innerHTML = "<h1>hej</h1>";
+	var xmlhttp = new XMLHttpRequest();
+        xmlhttp.onreadystatechange = function() {
+            if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+                document.getElementById("content").innerHTML = xmlhttp.responseText;
+            }
+        };
+        xmlhttp.open("GET", "userProfileEcho.php" , true);
+        xmlhttp.send();
 }
---
+++ php/userProfileEcho.php
@@ -0,0 +1,48 @@
+
+  
+
+  <?php 
+    echo '<link rel="stylesheet" type="text/css" href="css/userProfile.css">';
+    echo '<div id="container">';
+    echo 'Shit is real';
+    echo '<div id="profilePicture">'; 
+   	echo '<img id="userPicture">';
+    echo '</div>';
+
+    echo '<div id="profileName">';
+   	echo '<p id="profileText">Carl-Erik von Pappaspengar</p>';
+    echo '</div>';
+
+    echo '<div id="infoContainer">';
+    echo '<div id="infoTitles">';
+    echo '<p class="infoTitle"> Contact: </p>';
+      /*<!--  IF NOT NULL SENARE -->*/
+    echo '<p class="infoTitle"> City: </p>';
+    echo '<p class="infoTitle"> Age: </p>';
+    echo '<p class="infoTitle"> Gender: </p>';
+    echo '<p class="infoTitle"> Education: </p>';
+    echo '<p class="infoTitle"> Occupation: </p>';
+    echo '<p class="infoTitle"> Merits: </p>';
+    echo '</div>';
+
+
+    echo '<div id="infoText">';
+    echo '<p class="infoText"> ahmedson@gmail.com </p>';  
+   /* <!--  IF NOT NULL SENARE -->*/
+    echo '<p class="infoText"> Stockholm</p>';  
+    echo '<p class="infoText"> 23</p>';
+    echo '<p class="infoText"> Male</p>';
+    echo '<p class="infoText"> KTH Royal Institute of Technology</p>';
+    echo "<p class='infoText'> Mc'donalds</p>";
+    echo '<p class="infoText"> Shit is tuff</p>'; 
+    echo '</div>';
+    echo '</div>';
+
+    echo '<div id="descriptionContainer">';
+    echo '<p id="descriptionText">';
+    echo 'Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?';
+    echo '</p>';
+
+
+    echo '</div>';
+    ?>
