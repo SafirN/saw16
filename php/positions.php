ecc70450cec6a775e2ba801fcdaefafca07683f7
System Administrator <root@n162-p242.kthopen.kth.se>
2016-03-29 16:56:59 +0200

    fixar till positions

---
+++ php/positions.php
@@ -0,0 +1,37 @@
+<html>
+ <body>
+ <?php include("frame.php") ?> 
+  <link rel="stylesheet" type="text/css" href="css/positions.css">
+ <!-- Skapa en loop som skapar dessa taggar dynamiskt -->
+  <div id="container">
+	<div class="positionList"> 
+	 <div id="companyLogo">
+	 	Logo
+	 </div>
+
+	 <div id="companyName">
+	  <p class="companyTitle">Google</p>
+	 </div>
+	 <div id="positionContainer"> 
+	  <p class="position"> Software Developer in Google Play </p>
+	 </div>
+
+	</div>
+	<div class="positionList"> </div>
+	<div class="positionList"> </div>
+	<div class="positionList"> </div>
+	<div class="positionList"> </div>
+	<div class="positionList"> </div>
+	<div class="positionList"> </div>
+	<div class="positionList"> </div>
+	<div class="positionList"> </div>
+	<div class="positionList"> </div>
+  </div>
+
+
+
+ </body>
+
+
+
+</html>
