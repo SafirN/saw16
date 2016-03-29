a0fc8c1863f89ba108b2f9c1c2fd195607e4b8df
Safir <safir.najafi@gmail.com>
2016-03-29 17:25:28 +0200

    added javascript files

--- php/index.php
+++ php/index.php
@@ -4,6 +4,11 @@
  </title>
   <head>
    <link rel="stylesheet" type="text/css" href="css/index.css">
+   <script type="text/javascript" src="../javascript/login.js"></script>
+   <script type="text/javascript" src="../javascript/profile.js"></script>
+   <script type="text/javascript" src="../javascript/openPositions.js"></script>
+   <script type="text/javascript" src="../javascript/filledPositions.js"></script>
+   <script type="text/javascript" src="../javascript/friends.js"></script>
   </head>
  <body>
   <?php
