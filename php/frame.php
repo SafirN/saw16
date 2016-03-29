d2b2dbaf6a5591527fb98f25438f700ebe3f6b2d
Safir <safir.najafi@gmail.com>
2016-03-29 15:28:26 +0200

    Static content added

---
+++ php/frame.php
@@ -0,0 +1,17 @@
+<link rel="stylesheet" type="text/css" href="css/frame.css">
+<div id="banner"> 
+ <img src="img/banner.png" alt="banner">
+</div>
+<div id="bar">
+	<div id="navButtons">
+	 <button id="profile" class="barButton" type="button">Profile</button><button id="openPositions" class="barButton" type="button">Open positions</button><button id="filledPositions" class="barButton" type="button">Filled positions</button>
+	</div>
+	<div id="friendsButton">
+		<button id="friends" type="button">Friends</button>
+	</div>
+	<div id="searchField">
+		<form action="search.php" method="get">
+		 <input id="search" type="search" name="search" placeholder="Find people/organizations"><input id="submitSearch" type="submit" value="Go!">
+		</form>
+	</div>
+</div>
--- php/index.php
+++ php/index.php
@@ -6,5 +6,8 @@
    <link rel="stylesheet" type="text/css" href="css/index.css">
   </head>
  <body>
+  <?php
+   require "frame.php";
+  ?>
  </body>
 </html>
