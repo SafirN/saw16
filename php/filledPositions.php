67e8c1da88449e7b31a326078c839289d6fb4163
System Administrator <root@n162-p242.kthopen.kth.se>
2016-03-30 14:54:29 +0200

    filled positions

---
+++ php/filledPositions.php
@@ -0,0 +1,102 @@
+  <?php include("frame.php") ?>
+  <link rel="stylesheet" type="text/css" href="css/filledPositions.css">
+
+  <div id="container"> 
+   <ul> 
+    <li class="filledPositions">
+     <div id="companyLogo">
+	 	Logo
+	 </div>
+
+	 <div id="companyName">
+	  <p class="companyTitle">Google</p>
+	 </div>
+	 <div id="positionContainer"> 
+	  <p class="position"> Software Developer in Google Play </p>
+	 </div>
+
+
+	 <div id="filledBy">
+	 	<p class ="position"> Filled by: Ahmed ahmedson</p>
+	 </div>
+
+    </li>
+    <li class="filledPositions">
+     <div id="companyLogo">
+	 	Logo
+	 </div>
+
+	 <div id="companyName">
+	  <p class="companyTitle">Google</p>
+	 </div>
+	 <div id="positionContainer"> 
+	  <p class="position"> Software Developer in Google Play </p>
+	 </div>
+
+
+	 <div id="filledBy">
+	 	<p class ="position"> Filled by: Ahmed ahmedson</p>
+	 </div>
+
+    </li>
+    <li class="filledPositions">
+     <div id="companyLogo">
+	 	Logo
+	 </div>
+
+	 <div id="companyName">
+	  <p class="companyTitle">Google</p>
+	 </div>
+	 <div id="positionContainer"> 
+	  <p class="position"> Software Developer in Google Play </p>
+	 </div>
+
+
+	 <div id="filledBy">
+	 	<p class ="position"> Filled by: Ahmed ahmedson</p>
+	 </div>
+
+    </li>
+    <li class="filledPositions">
+     <div id="companyLogo">
+	 	Logo
+	 </div>
+
+	 <div id="companyName">
+	  <p class="companyTitle">Google</p>
+	 </div>
+	 <div id="positionContainer"> 
+	  <p class="position"> Software Developer in Google Play </p>
+	 </div>
+
+
+	 <div id="filledBy">
+	 	<p class ="position"> Filled by: Ahmed ahmedson</p>
+	 </div>
+
+    </li>
+    <li class="filledPositions">
+     <div id="companyLogo">
+	 	Logo
+	 </div>
+
+	 <div id="companyName">
+	  <p class="companyTitle">Google</p>
+	 </div>
+	 <div id="positionContainer"> 
+	  <p class="position"> Software Developer in Google Play </p>
+	 </div>
+
+
+	 <div id="filledBy">
+	 	<p class ="position"> Filled by: Ahmed ahmedson</p>
+	 </div>
+
+    </li>
+
+   </ul>
+
+
+
+
+  </div>
