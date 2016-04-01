754c0c84d04dc4b02e8f61ba527e95de6c5b808f
Safir <safir.najafi@gmail.com>
2016-04-01 13:40:31 +0200

    filledpos funkar nu

--- javascript/openPositions.js
+++ javascript/openPositions.js
@@ -36,7 +36,10 @@ function loadOpenPositions() {
            </li>';
      }
      retString += '</ul></div>';
-     document.getElementById("content").innerHTML = retString;
+      document.getElementById("search").innerHTML = '<form action="search.php" method="get">\
+      <input id="searchField" type="search" name="search" placeholder="Filter open positions"><input id="submitSearch" type="submit" value="Go!">\
+      </form>';
+      document.getElementById("content").innerHTML = retString;
     }
   });
 }
--- php/filledPositions.php
+++ php/filledPositions.php
@@ -5,7 +5,7 @@
   $conn = new PDO($serverName, $username, $pw);
   $conn->query("USE saw16"); // Selecting db
 
-  $prepared = $conn->prepare("SELECT company, mail, position, datetime, weeklyHours FROM filledPositions");
+  $prepared = $conn->prepare("SELECT filledPositions.company, filledPositions.position, filledPositions.datetime, filledPositions.weeklyHours, companyProfiles.companyPicture FROM filledPositions INNER JOIN companyProfiles ON filledPositions.company = companyProfiles.company");
   $prepared->execute();
   $result = array();
   while($row = $prepared->fetch(PDO::FETCH_ASSOC)){
@@ -13,24 +13,3 @@
   }
   print json_encode($result);
 ?>
-
-<!--<link rel="stylesheet" type="text/css" href="css/filledPositions.css">
-<div id="container"> 
- <ul> 
-  <li class="filledPositions">
-   <div id="companyLogo">
-				Logo
-			</div>
-			<div id="companyName">
- 			<p class="companyTitle">Google</p>
-			</div>
-			<div id="positionContainer"> 
- 			<p class="position"> Software Developer in Google Play </p>
-			</div>
-			<div id="filledBy">
-				<p class ="position"> Filled by: Ahmed ahmedson</p>
-			</div>
-		</li>
- </ul>
-</div>
--->
