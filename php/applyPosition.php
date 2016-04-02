b9fe23c4e661b17ca3be38da9a4e9568a55664d5
System Administrator <root@ziadgiliana.local>
2016-04-02 13:55:51 +0200

    little shit

--- php/applyPosition.php
+++ php/applyPosition.php
@@ -1,40 +1,57 @@
 <?php 
 	header("Content-Type: application/json", true);
-  	include("phpconf.php");
-  	$username = "Safir";
-  	//Create connection
-  	$conn = new PDO($serverName, $username, $pw);
-  	$conn->query("USE saw16"); // Selecting db
+  	
+	if(strcmp($_COOKIE['userType'], "user") == 0){
+		//Check if user is already applied
+		include("phpconf.php");
+		$username = "Safir";
+		//Create connection
+		$conn = new PDO($serverName, $username, $pw);
+		$conn->query("USE saw16"); // Selecting db
 
-	$prepared = $conn->prepare("SELECT freePositions FROM openPositions WHERE company = ? AND position = ?");
-	$prepared->bindParam(1, $_POST['company'], PDO::PARAM_STR, strlen($_POST['company']));
-    $prepared->bindParam(2, $_POST['position'], PDO::PARAM_STR, strlen($_POST['position']));
-    $prepared->execute();
+		$prepared = $conn->prepare("SELECT userMail FROM appliedPositions WHERE company = ? AND position = ? AND userMail = ?");
+		$prepared->bindParam(1, $_POST['company'], PDO::PARAM_STR, strlen($_POST['company']));
+		$prepared->bindParam(2, $_POST['position'], PDO::PARAM_STR, strlen($_POST['position']));
+		$prepared->bindParam(3, $_COOKIE['mail'], PDO::PARAM_STR, strlen($_COOKIE['mail']));
+		$prepared->execute();
+		$num_rows = $prepared->rowCount();
 
-    $freePositions = $prepared->fetch(PDO::FETCH_ASSOC);
+		if($num_rows == 0){ //Har ej sökt tidigare
+			$prepared = $conn->prepare("SELECT freePositions FROM openPositions WHERE company = ? AND position = ?");
+			$prepared->bindParam(1, $_POST['company'], PDO::PARAM_STR, strlen($_POST['company']));
+			$prepared->bindParam(2, $_POST['position'], PDO::PARAM_STR, strlen($_POST['position']));
+			$prepared->execute();
 
-    if($freePositions['freePositions'] > 0){
-		//Minska 1 på free position
-		/*$prepared = $conn->prepare("UPDATE openPositions SET freePositions = freePositions - 1 WHERE company = ? AND position = ? AND freePositions > 0");
-	    $prepared->bindParam(1, $_POST['company'], PDO::PARAM_STR, strlen($_POST['company']));
-	    $prepared->bindParam(2, $_POST['position'], PDO::PARAM_STR, strlen($_POST['position']));
-	    $prepared->execute();
-	    */
-	    //Hämta comp mail
-	    $prepared = $conn->prepare("SELECT mail FROM companyProfiles WHERE company = ?");
-	    $prepared->bindParam(1, $_POST['company'], PDO::PARAM_STR, strlen($_POST['company']));
-	    $prepared->execute();
-	    $companyMail = $prepared->fetch(PDO::FETCH_ASSOC);
-	    //Lägg in i applied positions 
-	    $prepared = $conn->prepare("INSERT INTO appliedPositions(company, position, companyMail, userMail) VALUES (?,?,?,?)");
-	   	$prepared->bindParam(1, $_POST['company'], PDO::PARAM_STR, strlen($_POST['company']));
-	    $prepared->bindParam(2, $_POST['position'], PDO::PARAM_STR, strlen($_POST['position']));
-		$prepared->bindParam(3, $companyMail['mail'], PDO::PARAM_STR, strlen($companyMail['mail']));
-	    $prepared->bindParam(4, $_COOKIE['mail'], PDO::PARAM_STR, strlen($_COOKIE['mail']));
-	    $prepared->execute();
+			$freePositions = $prepared->fetch(PDO::FETCH_ASSOC);
 
+			if($freePositions['freePositions'] > 0){
+				//Minska 1 på free position
+				/*$prepared = $conn->prepare("UPDATE openPositions SET freePositions = freePositions - 1 WHERE company = ? AND position = ? AND freePositions > 0");
+			    $prepared->bindParam(1, $_POST['company'], PDO::PARAM_STR, strlen($_POST['company']));
+			    $prepared->bindParam(2, $_POST['position'], PDO::PARAM_STR, strlen($_POST['position']));
+			    $prepared->execute();
+			    */
+			    //Hämta comp mail
+			    $prepared = $conn->prepare("SELECT mail FROM companyProfiles WHERE company = ?");
+			    $prepared->bindParam(1, $_POST['company'], PDO::PARAM_STR, strlen($_POST['company']));
+			    $prepared->execute();
+			    $companyMail = $prepared->fetch(PDO::FETCH_ASSOC);
+			    //Lägg in i applied positions 
+			    $prepared = $conn->prepare("INSERT INTO appliedPositions(company, position, companyMail, userMail) VALUES (?,?,?,?)");
+			   	$prepared->bindParam(1, $_POST['company'], PDO::PARAM_STR, strlen($_POST['company']));
+			    $prepared->bindParam(2, $_POST['position'], PDO::PARAM_STR, strlen($_POST['position']));
+				$prepared->bindParam(3, $companyMail['mail'], PDO::PARAM_STR, strlen($companyMail['mail']));
+			    $prepared->bindParam(4, $_COOKIE['mail'], PDO::PARAM_STR, strlen($_COOKIE['mail']));
+			    $prepared->execute();
+
+			}else{
+				print json_encode("Someone was faster then you to apply for this job");
+			}
+		}else{
+				print json_encode("Already applied for this job");
+		}
 	}else{
-		print json_encode("Someone was faster then you to apply for this job");
+		print json_encode("You cant apply for a job as an company");
 	}
 ?>
 
