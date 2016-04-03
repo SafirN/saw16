dba3575916a61597105d9fabdc03dec1ccc2aa13
Safir <safir.najafi@gmail.com>
2016-04-03 15:52:29 +0200

    Jahopp, nu blire kaos på riktigt

--- javascript/filledPositions.js
+++ javascript/filledPositions.js
@@ -25,11 +25,10 @@ function loadFilledPositions() {
   	 		retString += '</ul></div>';
   	 		document.getElementById("search").innerHTML = '<form id="filledPositionsSearchForm">\
         				<input id="searchField" type="search" name="search" placeholder="Filter filled positions">\
-        				<button id="submitSearch" type="button" onClick="searchFilledPositions(document.getElementById(\'searchField\').value)">Go!</button>\
+        				<button id="submitSearch" type="button" onClick="searchFilledPositions(document.getElementById(\'searchField\').value">Go!</button>\
         				</form>';
   			document.getElementById("content").innerHTML = retString;
 		}
-		
 	});
 }
 
--- php/index.php
+++ php/index.php
@@ -1,38 +1,36 @@
 <!DOCTYPE html>
 <html>
- <title>
-  SAW - jobs for murderers
- </title>
+  <title>
+    Jobbkontakt
+  </title>
   <head>
-   <script type="text/javascript" src="https://ajax.aspnetcdn.com/ajax/jQuery/jquery-1.12.0.min.js"></script>
-   <script type="text/javascript" src="../javascript/login.js"></script>
-   <script type="text/javascript" src="../javascript/profile.js"></script>
-   <script type="text/javascript" src="../javascript/openPositions.js"></script>
-   <script type="text/javascript" src="../javascript/filledPositions.js"></script>
-   <script type="text/javascript" src="../javascript/friends.js"></script>
-   <script type="text/javascript" src="../javascript/bar.js"></script>
-   <link rel="stylesheet" type="text/css" href="css/index.css">
-   <link rel="stylesheet" type="text/css" href="css/positions.css">
-   <link rel="stylesheet" type="text/css" href="css/userProfile.css">
-   <link rel="stylesheet" type="text/css" href="css/companyProfile">
-   <link rel="stylesheet" type="text/css" href="css/friends.css">
-   <link rel="stylesheet" type="text/css" href="css/filledPositions.css">
-   <link rel="stylesheet" type="text/css" href="css/frame.css">
+    <meta charset="UTF-8">
+    <script type="text/javascript" src="https://ajax.aspnetcdn.com/ajax/jQuery/jquery-1.12.0.min.js"></script>
+    <script type="text/javascript" src="../javascript/login.js"></script>
+    <script type="text/javascript" src="../javascript/profile.js"></script>
+    <script type="text/javascript" src="../javascript/openPositions.js"></script>
+    <script type="text/javascript" src="../javascript/filledPositions.js"></script>
+    <script type="text/javascript" src="../javascript/friends.js"></script>
+    <script type="text/javascript" src="../javascript/bar.js"></script>
+    <link rel="stylesheet" type="text/css" href="css/index.css">
+    <link rel="stylesheet" type="text/css" href="css/bar.css">
+    <link rel="stylesheet" type="text/css" href="css/userProfile.css">
+    <link rel="stylesheet" type="text/css" href="css/companyProfile.css">
+    <link rel="stylesheet" type="text/css" href="css/openPositions.css">
+    <link rel="stylesheet" type="text/css" href="css/filledPositions.css">
   </head>
   <body>
-   <div id="topBackground">
-    <div id="banner">
-     <img src="img/banner.png" alt="banner">
+    <div id="topBackground">
+      <div id="banner">
+        <img src="img/banner.png" alt="banner">
+      </div>
+      <div id="bar">
+      </div>
     </div>
-    <div id="bar">
-    </div>
-   </div>
-   <div id="contentDiv">
-  	 <div id="content">
+    <div id="content">
   	 <!-- Dynamic content is stored here! -->
-  	 </div>
-   </div>
-   <?php
+  	</div>
+  <?php
     $cookieName = "mail";
     echo '<script> loadBar(); </script>';
     if(!isset($_COOKIE[$cookieName])) {
@@ -40,6 +38,6 @@
     } else {
      echo '<script> loadProfile(); </script>';
     }
-   ?>
+  ?>
  </body>
 </html>
