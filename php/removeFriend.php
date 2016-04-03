aa8948347304a3571dc24a5c3c90a42835936f02
System Administrator <root@n191-p100.kthopen.kth.se>
2016-04-03 16:59:51 +0200

    remove follower

--- php/removeFriend.php
+++ php/removeFriend.php
@@ -5,7 +5,7 @@
   $conn = new PDO($serverName, $username, $pw);
   $conn->query("USE saw16"); // Selecting db
 
-  $prepared = $conn->prepare("DELETE FROM userFriends WHERE userFriends.mail = ? AND userFriends.friendMail = ?");
+  $prepared = $conn->prepare("DELETE FROM followers WHERE followers.mail = ? AND followers.followingMail = ?");
   $prepared->bindParam(1, $_COOKIE['mail'], PDO::PARAM_STR, strlen($_COOKIE['mail']));
   $prepared->bindParam(2, $_POST['data'], PDO::PARAM_STR, strlen($_POST['data']));
   $prepared->execute();
