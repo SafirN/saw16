b6efc325e0a0f1c84039e3b5d5a1767e0419709e
Safir <safir.najafi@gmail.com>
2016-04-04 14:19:29 +0200

    snark

--- javascript/friends.js
+++ javascript/friends.js
@@ -51,15 +51,22 @@ function searchFollowers() {
 		type: "GET",
 		dataType: "json",
 		success: function(data) {
-			var retStr = '<link rel="stylesheet" type="text/css" href="css/friendSearchResults.css"><div id="container"><ul>';
+			var retStr = '<ul>';
 			for(var i = 0; i < data.length; i++) {
-				retStr += '<li class="friendCell">\
-				<p class="friendName">' + data[i].name + '</p>\
-				<p class="date"> Friends since: ' + data[i].date + '</p>\
-				<button onClick="removeFriend(\'' + data[i].followingMail + '\')">Unfriend</button>\
-				</li>';
+				retString += '<li class="followingCell">\
+					<div>\
+						<p onClick="loadProfile(\'' + data[i].followingMail + '\')">' + data[i].name + '</p>\
+					</div>\
+					<div>\
+						<p>You have been a follower since</p>\
+						<p>' + data[i].date + '</p>\
+					</div>\
+					<div>\
+						<button onClick="removeFollow(\'' + data[i].followingMail + '\')">Unfollow</button>\
+					</div>\
+					</li>';
 			}
-			retStr += '</ul></div>';
+			retStr += '</ul>';
 			document.getElementById("search").innerHTML = '<form onsubmit="return searchFollowers()">\
         				<input id="searchField" type="text" name="search" placeholder="Filter friends"><input id="submitSearch" type="submit" onClick="searchFollowers()" value="Go!">\
         				</form>';
